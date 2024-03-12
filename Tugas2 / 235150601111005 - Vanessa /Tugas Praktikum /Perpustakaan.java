d3fefc08690b2c39ae2ca73fd52a0a72d3fdaf89
vanessadewi <160575734+vanessadewi@users.noreply.github.com>
2024-03-12 11:14:28 +0700

    bismillah 100

--- Tugas2 / 235150601111005 - Vanessa /Tugas Praktikum /Perpustakaan.java
+++ Tugas2 / 235150601111005 - Vanessa /Tugas Praktikum /Perpustakaan.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Perpustakaan {
     private String namaBuku;
     private int tahunBuku;
@@ -9,75 +13,74 @@ public class Perpustakaan {
         switch (pilihan) {
             case 1:
                 System.out.println("Teknologi\n");
-                tampilkanBuku("Clean code: A Handbook of Agile Software Craftsmansip", "Robert C. Martin", "Prentice Hall");
-                tampilkanBuku("Buku Pengantar Teknologi Informasi - Teknik Informatika", "Buhori Muslim", "Buku Pendidikan Deepublish");
-                tampilkanBuku("Computer Architecture: A Quantitative Approach", "David A., Patterson dan John L.", "Philadelphia");
-                tampilkanBuku("Design Patterns: Elements of Reusable Object-Oriented Software", "Erich Gamma, Richard Helm, Ralph Johnson, dan  John Vlissides", "Addison Wesley");
-                tampilkanBuku("The Pragmatic Programmer: Your Journey to Mastery", "Andrew Hunt dan David Thomas", "Addison Wesley");
-                tampilkanBuku("The Hidden Language of Computer Hardware and Software", "Charles Petzold", "Microsoft Press");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Teknologi.txt");
                 break;
             case 2:
                 System.out.println("Filsafat\n");
-                tampilkanBuku("Buku Filsafat Dunia Sophie", "Jostein Gaarder", "H.W. Wilson Company");
-                tampilkanBuku("Kutipan Buku Filosofi Teras", "Henry Manampiring", "PT Kompas Media Nusantara");
-                tampilkanBuku("Sophie's World: A Novel About The History of Philosophy", "jostein Gaarder", "Farrar, Straus and Giroux");
-                tampilkanBuku("Meditations", "Marcus Aurelius", "Noura Books Publishing");
-                tampilkanBuku("Critique of pure Reason (Kritik der reinen Vernunft)", "Immanuel Kant", "Johann Friedrich Hartknoch");
-                tampilkanBuku("Thus Spoke Zarathustra", "Friedrich Nietzsche", "Ernst Schmeitzner");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Filsafat.txt");
                 break;
             case 3:
                 System.out.println("Sejarah\n");
-                tampilkanBuku("Guns, Germs, and Steel", "Jared Diamond", "W. W. Norton & Company");
-                tampilkanBuku("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "McClelland & Stewart");
-                tampilkanBuku("A People's History of the United States", "Howard Zinn", "Harper & Row");
-                tampilkanBuku("The Silk Roads: A New History of the World", "Peter Frankopan", "Bloomsbury Publishing");
-                tampilkanBuku("The Rise and Fall of the Third Reich: A History of Nazi Germany", "William L. Shirer", "Simon & Schuster");
-                tampilkanBuku("1491: New Revelations of the Americas Before Columbus", " Charles C. Mann", " Alfred A. Knopf");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Sejarah.txt");
                 break;
-
             case 4:
                 System.out.println("Agama\n");
-                tampilkanBuku("Fikih Wanita: Panduan Praktis Muslimah Sehari-hari", "Dr. Husin Shahab.", "Aqwam");
-                tampilkanBuku("Bahasa Arab", "Anis", "Erlangga");
-                tampilkanBuku("Tafsir Al-Mishbah: Pesan, Kesan, dan Keserasian Al-Qur'an", "Prof. Dr. Muhammad Quraish Shihab.", "Lentera Hati");
-                tampilkanBuku("Islam dan Hak Asasi Manusia", "Prof. Dr. H. Achmad Ali.", "Gramedia Pustaka Utama");
-                tampilkanBuku("Islam: A Short History", "Karen Armstrong", "Modern Library");
-                tampilkanBuku("What Everyone Needs to Know About Islam", "John L. Esposito", "Oxford University Press");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Agama.txt");
                 break;
-
             case 5:
                 System.out.println("Psikologi\n");
-                tampilkanBuku("Magnet Kepribadian", "Laksana Candra", "Araska Publisher");
-                tampilkanBuku("Psikologi Remaja", "Asrori", "Rajawali Pers");
-                tampilkanBuku("Psikologi Pendidikan: Teori dan Praktik", "Dr. M. Arifin Budi Susilo.", "Jakarta Indeks");
-                tampilkanBuku("Psikologi Sosial: Perspektif Baru", "Dr. Sri Hastjarjo.", "Rineka Cipta");
-                tampilkanBuku("Mengungkap Kepribadian dengan Tes Psikologi", "Ananda Sukarlan.", "Media Nusa Creative");
-                tampilkanBuku("Buku Saku Psikologi", "Roy F. Baumeister dan Brad J. Bushman, diterjemahkan oleh Tim TBI UGM.", "Pbk");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Psikologi.txt");
                 break;
-
             case 6:
                 System.out.println("Politik\n");
-                tampilkanBuku("Keadaban Politik", "Kurniawan", "Intrans Publishing");
-                tampilkanBuku("Politik Kekuasaan", "Imron wassi", "Kepustakaan Populer Gramedia");
-                tampilkanBuku("Pengantar Ilmu Politik", "Miriam Budiardjo", "Gramedia Pustaka");
-                tampilkanBuku("Sistem Politik Indonesia: Kebijakan, Struktur, dan Proses", "Miriam Budiardjo", "CV Pustaka Setia");
-                tampilkanBuku("The Communist Manifesto", "Karl Marx dan Friedrich Engels", "Penguin Classics");
-                tampilkanBuku("The Conscience of a Conservative", "Barry Goldwater", "Victor Publishing Company");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Politik.txt");
                 break;
-
             case 7:
                 System.out.println("Fiksi\n");
-                tampilkanBuku("Laskar Pelangi", "Andrea Hirata", "Bentang Pustaka");
-                tampilkanBuku("Milea", "Pidi Baiq", "Pastel Books");
-                tampilkanBuku("Bumi Manusia", "Pramoedya Ananta Toer", "Hasta Mitra");
-                tampilkanBuku("Perahu Kertas", "Dewi Lestari", "Bentang Pustaka");
-                tampilkanBuku("Ayat-Ayat Cinta", " Habiburrahman El Shirazy", "Pustaka Arafah");
-                tampilkanBuku("Pulang ", "Tereliye", "Republika");
+                bacaDariFile("C:\\235150601111005-Vanessa\\Vanessa-PBO-PTI-A\\Tugas2\\Tugas Praktikum\\Fiksi.txt");
+                break;
+            default:
+                System.out.println("Kategori tidak valid.");
                 break;
         }
     }
 
-    public void tampilkanBuku(String judulBuku, String penulis, String penerbit) {
+    private void bacaDariFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts.length >= 3) {
+                    tampilkanBuku(parts[0], parts[1], parts[2]);
+                }
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File tidak ditemukan: " + e.getMessage());
+        }
+    }
+    
+    public void pinjamBuku() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan judul buku yang ingin dipinjam: ");
+        String judul = input.nextLine();
+        System.out.println("Buku " + judul + " berhasil dipinjam.");
+
+    }
+
+    public void kembaliBuku() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan judul buku yang ingin dikembalikan: ");
+        String judul = input.nextLine();
+        System.out.println("Buku " + judul + " berhasil dikembalikan.");
+        
+    }
+
+    private void tampilkanBuku(String judulBuku, String penulis, String penerbit) {
         System.out.println("Judul   : " + judulBuku);
         System.out.println("Penulis : " + penulis);
         System.out.println("Penerbit: " + penerbit);
