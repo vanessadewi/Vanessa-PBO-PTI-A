8754c5a62c61e116ff940705e034b9c3fb43a9a0
vanessadewi <160575734+vanessadewi@users.noreply.github.com>
2024-03-26 12:05:57 +0700

    bismillah 100

--- Tugas4 /Modul1Bab3 /LIngkaran.java
+++ Tugas4 /Modul1Bab3 /LIngkaran.java
@@ -1,1 +1,23 @@
+//1. Lakukan percobaan diatas dan benahi jika menemukan kesalahan! 
+
+//public class LIngkaranMain{  
+    //public static void main(String[] args){  
+    //lingkaran l = new lingkaran(3); 
+    //l.setTinggi(10); 
+    //l.displayMessage(); 
+    //lingkaran l2 = new lingkaran(4, 10); 
+    //l2.displayMessage();
+    //}
+//}
+
+//3. Pada class lingkaran tambahkan constrctor dengan tipe data String alas dan String tinggi, kemudian tambahkan 
+//method untuk melakukan parsing atau perubahan tipe data dari String menjadi integer. Setelah itu pada method main 
+//lakukan instansiasi objek dengan nama objek Lstring dengan memanggil constructor bertipe data String. Jelaskan!
+    public class LIngkaranMain {
+    public static void main(String[] args) {
+    lingkaran Lstring = new lingkaran("5", "10");
+    Lstring.displayMessage();
+    }
+}
+
 
