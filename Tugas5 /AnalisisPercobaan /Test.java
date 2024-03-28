4e3c1c2b109638152dbe3a7ec842d3b66ef59e25
vanessadewi <160575734+vanessadewi@users.noreply.github.com>
2024-03-28 07:30:09 +0700

    bismillah 100

--- Tugas5 /AnalisisPercobaan /Test.java
+++ Tugas5 /AnalisisPercobaan /Test.java
@@ -1,1 +1,24 @@
+//1. Lakukan percobaan diatas dan benahi jika menemukan kesalahan! 
 
+public class Test {
+    public static void main(String [] args) {
+    Student s1=new Student();
+    s1.setName("Enkapsulasi");
+    //s1.setMark("90");
+
+    s1.setMark(90); //1. Menghilangkan tanda "" untuk menggunakan nilai int bukan string
+
+    //2. Jika pada baris 6 s1.setName diubah menjadi s1.getName apa yang terjadi? jelaskan! 
+    //2. sebelum dirubah tadi error, setelah dirubah tidak error lagi 
+
+    //System.out.println("s1Name is "+s1.setName());
+    System.out.println("s1Name is "+s1.getName()); //1. merubah metod mutator menjadi metod aksesor
+
+    //System.out.println("s1Mark is "+s1.setMark());
+    System.out.println("s1Mark is "+s1.getMark()); //1. merubah metod mutator menjadi metod aksesor
+
+    //System.out.println("name dan mark "+name+" "+mark);
+    System.out.println("name dan mark "+s1.getName()+" "+s1.getMark());
+    }
+   }
+   
