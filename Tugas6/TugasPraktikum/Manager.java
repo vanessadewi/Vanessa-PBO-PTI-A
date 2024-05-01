294874ef85310154d992571d648b83118bc386af
vanessadewi <160575734+vanessadewi@users.noreply.github.com>
2024-05-01 14:29:00 +0700

    bismillah 100

---
+++ Tugas6/TugasPraktikum/Manager.java
@@ -0,0 +1,27 @@
+public class Manager extends Pekerja {
+    private String departemen;
+
+    public Manager() {}
+
+    public Manager(String departemen, double gaji, int tahunMasuk, int bulanMasuk, int tanggalMasuk, int jumlahAnak, String nama, String nik, boolean jenisKelamin, boolean menikah) {
+        super(gaji, tahunMasuk, bulanMasuk, tanggalMasuk, jumlahAnak, nama, nik, jenisKelamin, menikah);
+        this.departemen = departemen;
+    }
+
+    public double getPendapatan() {
+        return super.getPendapatan() + (super.getGaji() * 0.1);
+    }
+
+    public String toString() {
+        return super.toString() +
+                "\nDepartemen : " + getDepartemen();
+    }
+
+    public void setDepartemen(String departemen) {
+        this.departemen = departemen;
+    }
+
+    public String getDepartemen() {
+        return departemen;
+    }
+}
