c8e1c3bc932fd35a4ef972f60af253a27e30fb3e
vanessadewi <160575734+vanessadewi@users.noreply.github.com>
2024-05-08 15:40:07 +0700

    bismillah 100

---
+++ Tugas7/src/TugasPraktikum/Kue.java
@@ -0,0 +1,32 @@
+public abstract class Kue {
+    private String nama;
+    private double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public abstract double hitungHarga();
+
+    @Override
+    public String toString() {
+        return "Nama Kue: " + nama + ", Harga: " + harga;
+    }
+}
