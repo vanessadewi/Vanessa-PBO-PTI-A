9355a75ee0b72ec73cac62f80604ecaeeba0379a
vanessadewi <160575734+vanessadewi@users.noreply.github.com>
2024-05-14 17:49:43 +0700

    bismillah 100

---
+++ Tugas8/TugasPraktikum/PegawaiTetap.java
@@ -0,0 +1,27 @@
+package TugasPraktikum;
+
+public class PegawaiTetap extends Pegawai {
+    private double upah ;
+    public PegawaiTetap(String nama, String noKTP,
+                        double upah) {
+        super(nama, noKTP);
+        this.upah = upah;
+    }
+    public double getUpah() {
+        return upah;
+    }
+    public void setUpah(double upah) {
+        this.upah = upah;
+    }
+    @Override
+    public double gaji(){
+        return this.upah ;
+    }
+    @Override
+    public String toString() {
+        return "Pegawai Tetap : " +
+                this.getNama() + "\n" + super.toString()+ "\n" +
+                "Upah : " +upah+"\n" +
+                "Pendapatan : Rp"+gaji()+"\n";
+    }
+}
